4901d930fcff18453e43078a59e5b0a39dd2e2dd
AKIB <kabirakib.eng@gmail.com>
2026-08-11 19:44:51 +0600

    Added Square 2167A

---
+++ Square-2167A.cpp.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        if(a!=b || a!=c || a!=d || b!=c ||b!=d || c!=d){
+            cout<< "NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+        }
+
+    }
+}
