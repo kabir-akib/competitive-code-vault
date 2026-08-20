a521d34e1c8f702c2f6003c95b40cc829ca491fc
AKIB <kabirakib.eng@gmail.com>
2026-08-20 21:32:21 +0600

    Added Collatz Conjecture-2137A

---
+++ Collatz Conjecture-2137A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+
+        for(int i=0;i<a;i++){
+            b=b*2;
+        }
+        cout<<b<<endl;
+    }
+}
