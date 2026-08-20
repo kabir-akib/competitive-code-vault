2c5718bb28add827bd2731c6900f7b3de723722b
AKIB <kabirakib.eng@gmail.com>
2026-08-20 21:34:43 +0600

    Added The 67th Integer Problem-2218A

---
+++ The 67th Integer Problem-2218A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n>0){
+                if(n<67){
+        cout<<n+1<<endl;
+        }
+
+        else
+            cout<<n<<endl;
+        }
+        else{
+
+            cout<<n<<endl;
+
+            }
+    }
+}
