805987de29ee7a33afdc3d5290024a170ad3d779
AKIB <kabirakib.eng@gmail.com>
2026-08-27 20:43:16 +0600

    Added How Much Does Daytona Cost-1878A

---
+++ How Much Does Daytona Cost-1878A.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+
+        bool found=false;
+
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+
+            if(x==k){
+                found =true;
+            }
+        }
+        if(found)
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+}
