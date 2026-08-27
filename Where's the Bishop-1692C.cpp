d076a39388e4333e5c3df545768c6a307b7b6686
AKIB <kabirakib.eng@gmail.com>
2026-08-27 20:39:39 +0600

    Added Where's the Bishop-1692C

---
+++ Where's the Bishop-1692C.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+            int sum=0,x=0,y=0;
+        char arr[8][8];
+        for(int i=0 ;i<8;i++){
+            for(int j=0 ;j<8;j++){
+             cin>>arr[i][j];
+            }
+        }
+        for(int i=1 ;i<7;i++){
+            for(int j=1 ;j<7;j++){
+             if(arr[i-1][j-1]=='#' && arr[i-1][j+1]=='#' && arr[i+1][j-1]=='#' && arr[i+1][j+1]=='#'){
+                cout<<i+1<<" "<<j+1<<endl;
+             }
+            }
+        }
+
+    }
+}
